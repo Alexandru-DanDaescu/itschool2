a6209440bd6079b99567b2d6603e522b27f91f59
Alexandru Daescu <aksion244@gmail.com>
2023-09-01 14:02:35 +0300

    Session 11 ending.

---
+++ src/session11/practice/userservice/UserServiceImpl.java
@@ -0,0 +1,20 @@
+package session11.practice.userservice;
+
+public class UserServiceImpl implements UserService{
+
+
+    @Override
+    public void createUser(UserDTO userDTO) {
+
+    }
+
+    @Override
+    public UserDTO findUserById(long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteUser(long id) {
+
+    }
+}
