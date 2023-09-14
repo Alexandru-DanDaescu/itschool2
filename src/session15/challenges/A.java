3de713eef0a3dbb396a0e03bba5f479f3733495b
Alexandru Daescu <aksion244@gmail.com>
2023-09-14 21:28:32 +0300

    Session 15 ending.

---
+++ src/session15/challenges/A.java
@@ -0,0 +1,4 @@
+package session15.challenges;
+
+public class A {
+}
---
+++ src/session15/practice/musicstreamingservice/Artist.java
@@ -0,0 +1,18 @@
+package session15.practice.musicstreamingservice;
+
+import java.util.List;
+
+public class Artist {
+
+    private String artistName;
+
+    private List<Song> songList;
+
+
+
+
+
+
+
+
+}
