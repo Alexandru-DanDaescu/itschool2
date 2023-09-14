566ecde57427b38f5960e694ca22da2b5e0e18cd
Alexandru Daescu <aksion244@gmail.com>
2023-09-14 15:24:24 +0300

    Session 14 challenges(incomplete).

---
+++ src/session14/challenges/hashmap/Main.java
@@ -0,0 +1,171 @@
+package session14.challenges.hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //1.
+
+        Map<Integer, String> names = new HashMap<>();
+
+        addElements(names);
+
+        //2.
+
+        System.out.println(returnMapSize(names));
+
+        //3.
+
+        Map<Integer, String> firstMap = new HashMap<>();
+        firstMap.put(1, "der");
+        firstMap.put(2, "ab");
+        firstMap.put(3, "bc");
+        firstMap.put(4, "cd");
+
+        Map<Integer, String> secondMap = new HashMap<>();
+        secondMap.put(5, "raw");
+        secondMap.put(6, "chicken");
+        secondMap.put(7, "pork");
+        secondMap.put(8, "raw");
+
+        System.out.println(copyMappings(firstMap, secondMap));
+
+        //4.
+
+        Map<Integer, Double> anotherMap = new HashMap<>();
+        anotherMap.put(1, 34.34);
+        anotherMap.put(2, 43556.23);
+        anotherMap.put(3, 3445.543);
+
+        System.out.println(removeAllElements(anotherMap));
+
+        //5.
+        System.out.println(isMapEmpty(anotherMap));
+
+        //6.
+
+        Map<Integer, String> thirdMap = new HashMap<>();
+        thirdMap.put(1, "a");
+        thirdMap.put(2, "b");
+        thirdMap.put(3, "c");
+        thirdMap.put(4, "d");
+
+        Map<Integer, String> fourthMap = new HashMap<>();
+        fourthMap.put(5, "d");
+        fourthMap.put(6, "f");
+        fourthMap.put(7, "g");
+        fourthMap.put(8, "h");
+
+        System.out.println(copyMap(thirdMap, fourthMap));
+
+        //7.
+        Map<Integer, String> fifthMap = new HashMap<>();
+        fifthMap.put(1, "ame");
+        fifthMap.put(2, "read");
+        fifthMap.put(3, "no");
+        fifthMap.put(4, "yes");
+
+        System.out.println(mapContainsKey(fifthMap));
+
+        //8.
+
+        System.out.println(mapContainsValue(fifthMap));
+
+        //9.
+
+        Map<Integer, String> sixthMap = new HashMap<>();
+        sixthMap.put(1, "12");
+        sixthMap.put(2, "13");
+        sixthMap.put(3, "14");
+        sixthMap.put(4, "15");
+
+        Set<?> set = sixthMap.entrySet();
+
+        System.out.println("//9. " + set);
+
+        //10.
+
+        String value = sixthMap.get(3);
+        System.out.println("//10. " + value);
+
+        //11.
+
+        Set<Integer> keySet = sixthMap.keySet();
+        System.out.println("//11. " + keySet);
+
+        //12.
+
+        System.out.println("//12. " + sixthMap.values());
+
+
+    }
+
+    //1.
+    private static void addElements(Map<Integer, String> map) {
+        map.put(1, "John");
+        map.put(2, "Red");
+        map.put(3, "Mary");
+        System.out.println(map);
+    }
+
+    //2.
+    private static int returnMapSize(Map<Integer, String> map) {
+        return map.size();
+    }
+
+    //3.
+    private static Map<Integer, String> copyMappings(Map<Integer, String> firstMap, Map<Integer, String> secondMap) {
+        firstMap.putAll(secondMap);
+        return firstMap;
+    }
+
+    //4.
+
+    private static Map<Integer, Double> removeAllElements(Map<Integer, Double> map) {
+        map.clear();
+        return map;
+    }
+
+    //5.
+
+    private static boolean isMapEmpty(Map<Integer, Double> map) {
+        return map.isEmpty();
+    }
+
+    //6.
+    private static Map<Integer, String> copyMap(Map<Integer, String> firstMap, Map<Integer, String> secondMap) {
+        secondMap = new HashMap<>(firstMap);
+        return secondMap;
+    }
+
+    //7.
+
+    private static boolean mapContainsKey(Map<Integer, String> map) {
+        boolean found = false;
+        for (int i = 0; i < map.size(); i++) {
+            if (map.containsKey(i)) {
+                found = true;
+                break;
+            }
+        }
+        return found;
+    }
+
+    //8.
+    private static boolean mapContainsValue(Map<Integer, String> map) {
+        for (int i = 0; i < map.size(); i++) {
+            if (map.containsValue("ame") || map.containsValue("read") || map.containsValue("no")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //9.
+
+    //10.
+}
---
+++ src/session14/challenges/linkedlist/Main.java
@@ -0,0 +1,177 @@
+package session14.challenges.linkedlist;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        //1.
+        LinkedList<Integer> list = new LinkedList<>();
+
+        System.out.println(appendElements(list));
+
+        //2.
+
+        iterateThroughLinkedList(list);
+
+        //3.
+        System.out.println();
+        iterateFromASpecificPosition(list);
+
+        //4.
+        System.out.println();
+        iterateReverseOrder(list);
+
+        //5.
+        System.out.println();
+        System.out.println(insertElementAtPosition(list));
+
+        //6.
+        System.out.println(insertAtBeginningAndEnd());
+
+        //7.
+
+        System.out.println(insertAtFront());
+
+        //8.
+        System.out.println(insertAtEnd());
+
+        //9.
+
+        System.out.println(insertAtSpecifiedPosition());
+
+        //10.
+
+        LinkedList<Boolean> linkedList = new LinkedList<>();
+        linkedList.add(true);
+        linkedList.add(false);
+        linkedList.add(false);
+        linkedList.add(false);
+        linkedList.add(false);
+        linkedList.add(false);
+        linkedList.add(true);
+
+        getFirstAndLastOccurence(linkedList);
+
+
+        //11.
+
+        displayElementsAndPosition(linkedList);
+
+
+    }
+
+    //1.
+    private static LinkedList<Integer> appendElements(LinkedList<Integer> list) {
+        list.add(1);
+        list.add(23);
+        list.add(34);
+        list.add(45);
+        list.add(56);
+        return list;
+    }
+
+    //2.
+    private static void iterateThroughLinkedList(LinkedList<Integer> list) {
+        for (int i : list) {
+            System.out.print(i + " ");
+        }
+
+    }
+
+    //3.
+
+    private static void iterateFromASpecificPosition(LinkedList<Integer> list) {
+        Iterator<Integer> iterator = list.listIterator(3);
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next() + " ");
+        }
+    }
+
+    //4.
+
+    private static void iterateReverseOrder(LinkedList<Integer> list) {
+        for (int i = list.size() - 1; i >= 0; i--) {
+            System.out.print(list.get(i) + " ");
+        }
+    }
+
+    //5.
+
+    private static LinkedList<Integer> insertElementAtPosition(LinkedList<Integer> list) {
+        list.add(0, 454);
+        list.add(3, 4523);
+
+        return list;
+    }
+
+    //6.
+
+    private static LinkedList<String> insertAtBeginningAndEnd() {
+        LinkedList<String> words = new LinkedList<>();
+        words.add("asd");
+        words.add("sfd");
+        words.add("dgf45");
+        words.addFirst("hey");
+        words.addLast("No");
+
+        return words;
+    }
+
+    //7.
+    private static LinkedList<Double> insertAtFront() {
+        LinkedList<Double> doubles = new LinkedList<>();
+        doubles.add(23.23);
+        doubles.add(345.34);
+        doubles.add(34.45);
+        doubles.offerFirst(43435.34345);
+
+        return doubles;
+    }
+
+    //8.
+    private static LinkedList<String> insertAtEnd() {
+        LinkedList<String> strings = new LinkedList<>();
+        strings.add("deader");
+        strings.add("sum");
+        strings.add("agh");
+        strings.offerLast("A");
+
+        return strings;
+    }
+
+    //9.
+    private static LinkedList<String> insertAtSpecifiedPosition() {
+        LinkedList<String> strings = new LinkedList<>();
+        strings.add("rang");
+        strings.add("rad");
+        strings.add("sad");
+        strings.add("moo");
+        strings.add("eh");
+        strings.add("red");
+        strings.add(3, "haha");
+        strings.add(1, "raw");
+
+        return strings;
+    }
+
+    //10.
+
+    private static void getFirstAndLastOccurence(LinkedList<Boolean> linkedList) {
+        System.out.println(linkedList.getFirst() + " " + linkedList.getLast());
+    }
+
+    //11.
+
+    private static void displayElementsAndPosition(LinkedList<Boolean> linkedList) {
+        for (int i = 0; i < linkedList.size(); i++) {
+            System.out.println(linkedList.get(i) + " At index: " + i);
+        }
+    }
+
+
+}
