14273674523468ec62638bbe7760a63a6de6fc38
Alexandru Daescu <aksion244@gmail.com>
2023-09-21 11:20:39 +0300

    Session 16 challenges.

--- src/session16/challenges/genericitemfiltering/Main.java
+++ src/session16/challenges/genericitemfiltering/Main.java
@@ -1,12 +1,31 @@
 package session16.challenges.genericitemfiltering;
 
 
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
 
 public class Main {
 
     public static void main(String[] args) {
-        //TODO : finish this exercise
+
+        ListFilter<String> listFilter = (list, predicate) -> {
+            List<String> filteredList = new ArrayList<>();
+            for(String word : list){
+                if(predicate.test(word)){
+                    filteredList.add(word);
+                }
+            }
+            return filteredList;
+        };
+
+        Predicate<String> greaterThanSeven = string -> string.length() > 7;
+
+        List<String> randomWords = Arrays.asList("Hello","Something","Nuts","Randomizer","Substitute","Crush");
+
+        System.out.println("Original list: " + randomWords);
+        System.out.println("Updated list: " + listFilter.filterItems(randomWords,greaterThanSeven));
 
     }
 }
